f72d4936eaadbb3384390eb8e6d0d988e4691f60
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-06-05 10:23:16 -0500

    operadores
    clase de operadores

---
+++ cosascurso.py
@@ -0,0 +1,48 @@
+# Tipos de datos
+# Tipo numericos:
+int ="enteros"
+float = "decimales"
+'''
+podemos saber el tipo de dato de una variable con:
+type(variable)
+o puede ser hasta de un numero con type(#)
+'''
+# Tipo texto
+str = "representa secuencias o cadenas de caracteres""Lo que quiera se puede tener en string, hasta los numeros si se ponen entre comillas simples o dobles"
+# Tipos booleanos
+bool = "True o False""para las preguntas que se pueden responder de forma simple""tiene que escribirse con mayuscula la primera letra"
+
+# Calculando valores. Operadores y expresiones
+"""
++ = suma
+- = resta
+* = multiplicacion
+/ = division
+** = potencia
+// = division entera
+% = residuo
+
+"""
+'''
+Tambien estan los 
+< = menor que
+> = mayor que
+<= = menor o igual que
+>= = mayor o igual que
+== = igual que
+!= = distinto que
+
+Estos siempre entregan un boolean
+
+Tambien estan con la negación
+siempre dan un boolean 
+not = negacion
+esta el y o no
+y = and  cuandos los dos son verdaderos
+o = or cuando uno de los dos es verdadero
+'''
+# Hay operadores para str
+'''
++ = concatenar
+* = repetir
+'''
